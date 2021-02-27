040868d6a2c3cc5f0b356eb9baa3bd15aef96872
Pavel Devyataykin <vip_pavel777@mail.ru>
2021-02-27 21:13:10 +0700

    полиморфизм1

---
+++ Tetris/ConsoleApp1/Human.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    abstract class Human
+    {
+        protected int age;
+        protected int height;
+
+        public void Set(int a)
+        {
+            age = a;
+        }
+
+        public void Set(int a, int h)
+        {
+            age = a;
+            height = h;
+        }
+
+        //public virtual void Write()
+        //{
+        //    Console.WriteLine($"age: {age}. height: {height}");
+        //}
+        public abstract void Write();
+    }
+}
---
+++ Tetris/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human[] humans = new Human[2];
+            humans[0] = new Employee(12, 100000);
+            humans[1] = new Employee(22, 100000000);
+            
+            foreach(Human h in humans){
+                h.Write();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
