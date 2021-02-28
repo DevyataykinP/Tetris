bd319f844637d5cf0c9fb9e5584937eb4971e920
Pavel Devyataykin <vip_pavel777@mail.ru>
2021-02-28 20:48:13 +0700

    класс Random

---
+++ Tetris/Randomizer/random.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomizer
+{
+    class random
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+           
+            for(int i = 0; i<20; i++)
+            {
+                int value = rand.Next(0, 5);
+                Console.WriteLine(value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
