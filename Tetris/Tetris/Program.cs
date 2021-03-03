eb76dafffb8c0a28e9d9976c0afddf37457b44d0
Pavel Devyataykin <vip_pavel777@mail.ru>
2021-03-03 21:17:10 +0700

    класс Field

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -11,9 +11,8 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.SetWindowSize(40, 30);
-            Console.SetBufferSize(40, 30);
-
+            Console.SetWindowSize(Field.WIDTH, Field.HEIGHT);
+            Console.SetBufferSize(Field.WIDTH, Field.HEIGHT);
 
             FigureGenerator generator = new FigureGenerator(20, 0, '#');
             Figure currentFigure=generator.GetNewFigure();
@@ -42,6 +41,9 @@ namespace Tetris
                     break;
                 case ConsoleKey.DownArrow:
                     currentFigure.TryMove(Direction.DOWN);
+                    break;
+                case ConsoleKey.Spacebar:
+                    currentFigure.TryRotate();
                     break;
             }
 
