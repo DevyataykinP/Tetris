f62b5ac9024d3da7a1d384b9fca7504018278e23
Pavel Devyataykin <vip_pavel777@mail.ru>
2021-04-10 15:39:34 +0700

    lesson730

---
+++ skillbox/skillbox/worker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skillbox
+{
+    class worker
+    {
+        public string Position;
+        public uint Salary;
+        public string FirstName;
+        public string LastName;
+        public DateTime DateOfBirth;
+        public string Print()
+        {
+            return $"Должность: {Position} Зарплата: {Salary} Имя: {FirstName} Фамилия:{LastName} Дата рождения: {DateOfBirth.ToShortDateString()}";
+        }
+
+    }
+}
